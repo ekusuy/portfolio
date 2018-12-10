050a8d0dcfc1368a0eafd24e1f637c70f3ba6938
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-10 14:27:45 +0900

    jsをリファクタリング

--- vendor/assets/javascripts/resize.js
+++ vendor/assets/javascripts/resize.js
@@ -1,28 +1,28 @@
-
+"use strict";
 $(window).on('load resize', function(){
   trimming('.photo');//ここにトリミングしたい枠の種類
-})
-function trimming ($photoBox) {
-  jQuery($photoBox).each(function(){
-    var box = jQuery(this);
-    var i = jQuery('img',this);
-    var box_w =$(box).width();
-    var box_h =$(box).height();
-    var i_w =$(i).width();
-    var i_h =$(i).height();
-    var i_par =i_w / i_h; //画像の縦横比
-    var box_par =box_w / box_h; //画像を包むボックスの縦横比
-  //画像の縦横比と枠の縦横比を比べて分岐させ違うトリミングをする
+});
+function trimming (amazon_img) {
+  $(amazon_img).each(function(){
+    let box = $(this);
+    let i = $('img',this);
+    let box_w =$(box).width();
+    let box_h =$(box).height();
+    let i_w =$(i).width();
+    let i_h =$(i).height();
+    let i_par =i_w / i_h; //画像の縦横比
+    let box_par =box_w / box_h; //画像を包むボックスの縦横比
+    //画像の縦横比と枠の縦横比を比べて分岐させ違うトリミングをする
     if (i_par > box_par) { //画像が枠より横長の場合高さ100%で幅左右を切る
-        $(i).css({
-            "width": "auto",
-            "height":"100%"
-        });
+      $(i).css({
+        "width": "auto",
+        "height":"100%"
+      });
     }else{
-        $(i).css({//画像が枠より縦長または同じの場合幅100%にして高さの上下を切る
-            "width": "100%",
-            "height":"auto"
-        });
+      $(i).css({//画像が枠より縦長または同じの場合幅100%にして高さの上下を切る
+        "width": "100%",
+        "height":"auto"
+      });
     }
   });
 }
