088f9b21583414e3c04400192c27ac41d6012d37
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-10 11:32:29 +0900

    specファイル追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show
+    # FIXME: @userの実装はtwitterログイン実装後に変更する必要あり
+    @user = User.find(params[:id])
+    @contents = Content.where(user_id: @user.id) if @user
+    @content = Content.find(params[:id])
+  end
+end
