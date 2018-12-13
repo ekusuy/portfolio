eb1c82d646492b1c8ad3196810560fcf02fa7566
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-12-13 10:51:23 +0900

    routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
   get '/login', to: 'user_sessions#login'
-  root 'user_sessions#login'
-  resources :users, only: %i[show]
   get 'users', to: 'users#index'
+  resources :users, only: %i[show]
+  root 'user_sessions#login'
 end
