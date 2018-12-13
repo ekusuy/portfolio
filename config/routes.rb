8c2a5b543432878383268719eb6a4498dedb3cfb
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-13 18:27:34 +0900

    体裁面の修正とログイン後の遷移先をshowへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback"
-  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-  get '/login', to: 'user_sessions#login'
-  get 'users', to: 'users#index'
+  get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
   resources :users, only: %i[show]
   root 'user_sessions#login'
 end
