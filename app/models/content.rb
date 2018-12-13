c851faf6567285ed3499c870d8fec96de5264f9f
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-12-13 13:22:14 +0900

    最新のdevelopをpull

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,10 +1,7 @@
 class Content < ApplicationRecord
-<<<<<<< HEAD
-=======
   belongs_to :user
 
   def content_img
     amazon_img.present? ? amazon_img : 'sample.jpg'
   end
->>>>>>> bcbbf22cab4ab1267cab2c3c86097e0d93f243fe
 end
