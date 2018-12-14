5113215e8b856fcde39938f99da107b5d85e539b
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-14 16:20:00 +0900

    Sampleページだったらエディットページへのリンクを生成するメソッドを追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -4,4 +4,9 @@ class Content < ApplicationRecord
   def content_img
     amazon_img.present? ? amazon_img : 'sample.jpg'
   end
+
+  def create_link
+    amazon_link.present? ? amazon_link :
+    Rails.application.routes.url_helpers.edit_user_content_path(user_id, position)
+  end
 end
