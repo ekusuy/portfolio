6116c8f88c355e3f388ae44846db53b38060490f
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-14 12:36:32 +0900

    amazon用gemとrouting追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback"
   get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
-  resources :users, only: %i[show]
+  resources :users, only: %i[show] do
+    resources :contents, param: :position, only: %i[edit update]
+  end
   root 'user_sessions#login'
 end
