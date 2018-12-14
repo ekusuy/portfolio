b83f9e132c8188bd575e92685f5cc67ee4046255
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-14 16:17:50 +0900

    自動検索用js追加

---
+++ app/assets/javascripts/incremental_search.js
@@ -0,0 +1,29 @@
+$(window).on('load', function() {
+  let timer;
+  let preInput = "";
+
+  ajaxSearch = function(input) {
+    $.ajax({
+      url: $(location).attr('pathname'),
+      type: "GET",
+      dataType: 'script',
+      data: ("keyword=" + input)
+    });
+  };
+
+  triggerAjax = function(input) {
+    //文字列が変更ときのみメソッド実行する
+    if(preInput !== input){
+      clearTimeout(timer);
+      // 処理を500ms毎に実行
+      timer = setTimeout(ajaxSearch, 500, input);
+    };
+  };
+
+  $('#book_search').on('keyup', function() {
+    // 文字列の先頭と末尾の空白を削除
+    input = $.trim($(this).val());
+    triggerAjax(input);
+    preInput = input;
+  });
+});
