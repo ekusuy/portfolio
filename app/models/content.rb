eb2f5ec5723a7697b1e63b9f4c5e9b9410aa1043
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-17 17:22:39 +0900

    url_helperをインクルードする処理に変更

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,5 @@
 class Content < ApplicationRecord
+  include Rails.application.routes.url_helpers
   belongs_to :user
 
   def content_img
@@ -6,7 +7,6 @@ class Content < ApplicationRecord
   end
 
   def create_link
-    amazon_link.present? ? amazon_link :
-    Rails.application.routes.url_helpers.edit_user_content_path(user_id, position)
+    amazon_link.present? ? amazon_link : edit_user_content_path(user_id, position)
   end
 end
