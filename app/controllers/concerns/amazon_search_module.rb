1b241b32fd58c2356874bf7303dfd3f2d8cf4847
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-18 11:33:01 +0900

    検索処理のリファクタリング

--- app/controllers/concerns/amazon_search_module.rb
+++ app/controllers/concerns/amazon_search_module.rb
@@ -2,30 +2,36 @@ module AmazonSearchModule
   extend ActiveSupport::Concern
 
   def search_item_from_amazon(search_word)
-    if search_word.present?
-      # デバックログ出力するために記述
-      Amazon::Ecs.debug = true
+    search_results = []
+    return search_results if search_word.blank?
+    # デバックログ出力オン
+    Amazon::Ecs.debug = true
+    # Amazon::Ecs::Responseオブジェクトの取得
+    amazon_items = search_option(search_word)
+    # 商品のタイトル,画像URL, 詳細ページURLの取得
+    search_results = get_item_detail(amazon_items)
+  end
 
-      # Amazon::Ecs::Responseオブジェクトの取得
-      results = Amazon::Ecs.item_search(
-        search_word,
-        search_index:  'All',
-        dataType: 'script',
-        response_group: 'ItemAttributes, Images',
-        country:  'jp',
+  def search_option(search_word)
+    Amazon::Ecs.item_search(
+      search_word,
+      search_index:  'All',
+      dataType: 'script',
+      response_group: 'ItemAttributes, Images',
+      country:  'jp',
+    )
+  end
+
+  def get_item_detail(results)
+    search_item_details = []
+    results.items.each do |item|
+      result = Search_item.new(
+        item.get('ItemAttributes/Title'),
+        item.get('LargeImage/URL'),
+        item.get('DetailPageURL'),
       )
-
-      # 商品のタイトル,画像URL, 詳細ページURLの取得
-      search_results = []
-      results.items.each do |item|
-        result = Search_item.new(
-          item.get('ItemAttributes/Title'),
-          item.get('LargeImage/URL'),
-          item.get('DetailPageURL'),
-        )
-        search_results << result
-      end
-      search_results
+      search_item_details << result
     end
+    search_item_details
   end
 end
