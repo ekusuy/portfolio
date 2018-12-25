fbeebdd1127742d23628c15176728806e0411239
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-25 11:06:59 +0900

    update処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
   def show
-    @user = User.find(params[:id])
     @contents = Content.where(user_id: @user.id) if @user
   end
 
@@ -8,4 +8,23 @@ class UsersController < ApplicationController
     # 最新の登録コンテンツをユーザー毎に取得
     @contents = Content.order(updated_at: :desc).to_a.uniq { |c| c.user_id }
   end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors.messages }, status: 422
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :title
+      )
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
