278c0923d8535e93703d51c518bb955c2b9cf21d
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-25 12:14:44 +0900

    リファクタリング

--- app/assets/javascripts/edit_title.js
+++ app/assets/javascripts/edit_title.js
@@ -2,11 +2,9 @@
 'use strict';
 $(function(){
   $(document).on('click','.edit_title', function() {
-    let title_area =  $('#title_text'); // タイトル表示エリア
     let edit_area =  $('#edit_area'); // タイトル編集フォーム
     let update_url = $(location).attr('pathname'); // updateのURLを形成
     let error_msg = $('#user-error-message'); // エラーメッセージ表示場所
-    let title_submit = $('.title_submit') // 編集完了ボタン
     //AddClassされている場合はajaxを実行
     if($('.edit_content').length){
       $.ajax({
@@ -19,9 +17,7 @@ $(function(){
       }).done(function (data) {
         // 返却されたJSONをもとに書き換え
         $('#title_text h2').text(data['title']);
-        title_area.toggle();
-        edit_area.toggle();
-        title_submit.toggle();
+        form_toggle();
         error_msg.hide();
       }).fail(function(data){
         // エラーの場合はエラーメッセージを表示
@@ -34,9 +30,13 @@ $(function(){
     //1回目のエディットボタン押下
     }else{
       // フォームの表示・非表示を切り替え、編集フォームにクラスを付与
-      title_area.toggle();
-      edit_area.toggle().addClass("edit_content");
-      title_submit.toggle();
+      form_toggle();
+      edit_area.addClass("edit_content");
     }
   });
 });
+function form_toggle(){
+  $('#title_text').toggle();
+  $('#edit_area').toggle();
+  $('.title_submit').toggle();
+}
