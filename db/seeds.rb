b5b9ef56780204b3bac92abbc1f00d49cb8dc434
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-12-25 13:19:06 +0900

    不要な記述を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,6 @@
 # 50人のユーザーそれぞれが、(position: "1")のcontentのみupdateする
 # 50.times do |n|
 #   @user = User.find_by(twitter_id: "twitter_id_#{n}")
-#   (1..9).each { |m| @user.contents.build(position: m) }
 #   @user.contents.update(
 #     position: "1",
 #     amazon_link: "https://www.amazon.co.jp/Kindle-Paperwhite%E3%80%81%E9%9B%BB%E5%AD%90%E6%9B%B8%E7%B1%8D%E3%83%AA%E3%83%BC%E3%83%80%E3%83%BC-%E7%AC%AC7%E4%B8%96%E4%BB%A3-%E3%80%81Wi-Fi-%E3%80%814GB%E3%80%81%E3%83%96%E3%83%A9%E3%83%83%E3%82%AF/dp/B00QJDOM6U?psc=1&amp;SubscriptionId=AKIAI7XJ5SLOQO4ZHZ5A&amp;tag=naoki0b-22&amp;linkCode=xm2&amp;camp=2025&amp;creative=165953&amp;creativeASIN=B00QJDOM6U",
