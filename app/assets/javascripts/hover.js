9bd79b18b504fd9c2d6cb639d98f0840493e006d
Yusuke Omi <ekusuy.omi@gmail.com>
2018-12-26 14:04:02 +0900

    hover.jsの削除とページめくり幅修正

--- app/assets/javascripts/hover.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  //各画像左上のeditページリンクにマウスを乗せたら発動
-  $('.photo i').hover(function() {
-    //ページめくりクラスを付与
-    $('.photo').addClass("hvr-curl-top-left");
-  //ここにはマウスを離したときの動作を記述
-  }, function() {
-  });
-});
