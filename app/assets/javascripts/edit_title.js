97cee644451ba2f72cfcd898117aa45a6786f4f9
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-12-26 16:26:42 +0900

    シェア機能の修正

--- app/assets/javascripts/edit_title.js
+++ app/assets/javascripts/edit_title.js
@@ -1,42 +1,44 @@
 /*jslint node: true */
-'use strict';
-$(function(){
-  $(document).on('click','.edit_title', function() {
-    let edit_area =  $('#edit_area'); // タイトル編集フォーム
-    let update_url = $(location).attr('pathname'); // updateのURLを形成
-    let error_msg = $('#user-error-message'); // エラーメッセージ表示場所
+"use strict";
+$(function() {
+  $(document).on("click", ".edit_title", function() {
+    let edit_area = $("#edit_area"); // タイトル編集フォーム
+    let update_url = $(location).attr("pathname"); // updateのURLを形成
+    let error_msg = $("#user-error-message"); // エラーメッセージ表示場所
     //AddClassされている場合はajaxを実行
-    if($('.edit_content').length){
+    if ($(".edit_content").length) {
       $.ajax({
-        type: 'PATCH',
+        type: "PATCH",
         url: update_url,
         // 編集フォームの値を送信
         data: {
-          user:{title: edit_area.val()}
+          user: { title: edit_area.val() }
         }
-      }).done(function (data) {
-        // 返却されたJSONをもとに書き換え
-        $('#btn_title').text(data['title']);
-        form_toggle();
-        error_msg.hide();
-      }).fail(function(data){
-        // エラーの場合はエラーメッセージを表示
-        let errors = JSON.parse(data.responseText).errors;
-        $.each(errors, (function(column_name, msg){
-          error_msg.text(column_name + msg);
-        }));
-        error_msg.show();
-      });
-    //1回目のエディットボタン押下
-    }else{
+      })
+        .done(function(data) {
+          // 返却されたJSONをもとに書き換え
+          $("#btn_title").text(data["title"]);
+          form_toggle();
+          error_msg.hide();
+        })
+        .fail(function(data) {
+          // エラーの場合はエラーメッセージを表示
+          let errors = JSON.parse(data.responseText).errors;
+          $.each(errors, function(column_name, msg) {
+            error_msg.text(column_name + msg);
+          });
+          error_msg.show();
+        });
+      //1回目のエディットボタン押下
+    } else {
       // フォームの表示・非表示を切り替え、編集フォームにクラスを付与
       form_toggle();
       edit_area.addClass("edit_content");
     }
   });
 });
-function form_toggle(){
-  $('#title_text').toggle();
-  $('#edit_area').toggle();
-  $('.title_submit').toggle();
+function form_toggle() {
+  $("#title_text").toggle();
+  $("#edit_area").toggle();
+  $(".title_submit").toggle();
 }
